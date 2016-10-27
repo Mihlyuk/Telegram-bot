ecd92ea6e1728e8d57941b4cbd0a33299afca4be
Konstantin Mikhliuk <mikhliuk.k@gmail.com>
2016-10-27 18:19:21 +0300

    katyusha

--- main.rb
+++ main.rb
@@ -5,8 +5,8 @@ token = '282969345:AAEkW0070hAfVNlqKasIPvNYF076ANBSoMs'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Приветик, #{message.from.first_name}")
+      when 'Привет, Катюша'
+        bot.api.send_message(chat_id: message.chat.id, text: "Приветики, #{message.from.first_name}")
       when '/stop'
         bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
